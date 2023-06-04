359438049712b01f85b29a4c6afb376d61aa22c1
Kirandeep singh <kirankalsi.dev@gmail.com>
2023-06-05 00:37:45 +0530

    dark mode has been implemeted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import './App.css';
+//import About from './components/About';
 import NavBar from './components/Navbar';
 import Text from './components/Text';
 
 function App(props) {
+  const [mode, setMode] = useState('light');
+  const[btnText, setBtnText]= useState('Enable Dark Mode');
+
+ const toggleMode = ()=>{
+  if(mode === "light"){
+    setMode('dark');
+    setBtnText('Enable Light Mode');
+  }else{
+    setMode('light')
+    setBtnText('Enable Dark Mode')
+  }
+ }
+
   return (
     <div className="App">
-      <NavBar title="TextUtils"/>
+      <NavBar title="TextUtils" mode={mode} toggleMode={toggleMode} buttonText = {btnText}/>
+      {/* <About /> */}
       <div className='container my-3'>
          <Text heading="Enter the text for analyze"/>
       </div>
--- src/components/Text.js
+++ src/components/Text.js
@@ -13,12 +13,17 @@ function Text(props){
    }
     return(
     <>
-        <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="3"></textarea>
-            </div>
-            <button className="btn btn-primary" onClick={handleClick}>Convert to Uppercase</button>
-
+        <div className="container">
+            <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="3"></textarea>
+                </div>
+                <button className="btn btn-primary" onClick={handleClick}>Convert to Uppercase</button>
+        </div>
+        <div className="conatiner my-3">
+          <h1>Your text summary</h1>
+          <p>{text.split(' ').length} words and {text.length} characters</p>
+        </div>
     </>    
     )
 }
